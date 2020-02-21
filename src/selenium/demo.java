37826b1be648f5558779ba621e01adb55910b67b
HP <HP@DESKTOP-G6LV5BI>
2020-02-21 18:05:38 +0530

    commited

---
+++ src/selenium/demo.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", ".\\software\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://actitime.jmr.co.za/actitime/login.do");
+		Thread.sleep(2000);
+	}
+
+}
